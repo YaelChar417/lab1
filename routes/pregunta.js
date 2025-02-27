ebe8d46183b3773cadf3a6010e33d8990ca266a8
Yael Charles <yaelcharles417@gmail.com>
2025-02-26 22:59:23 -0600

    Update: ruta /preguntas finalizada, describiendo el archivo package.json

--- routes/pregunta.js
+++ routes/pregunta.js
@@ -18,9 +18,15 @@ const html_header = `<!DOCTYPE html>
 <div class="card-content">
     <h1>Preguntas a responder:</h1>
     <h2>Describe el archivo package.json:</h2>
-    <p></p>
+    <p>El package.json es un archivo de mucha importancia cuando se usa node.js, ya que este se necesita para definir  y manejar las características de un proyecto desarrollado con este entorno. Como lo indica su nombre tiene una estructura en formato JSON, JavaScript Object Notation y contiene datos como el nombre, version, licencia, autor y mucho más del proyecto.</p>
+    <p>En el package.json también se pueden definir scripts que faciliten la ejecución de tares comunes en el proyecto ,como iniciar el servidor, compilar codigo o ejecutar pruebas, etc, que pueden ser llamados con el comando <strong>npm run </strong></p>
+    <p>Por último, generalmente el archivo package.json no se incluye en los repositorios de github incluso cuando es muy importante, esto se debe principalmente a dos razones:</p>
+    <ul>
+        <li>Reducir el tamaño del repositorio, ya que es un archivo que esta en constante cambio</li>
+        <li>Facil de generar, cualquiera puede ejecutar el comando npm install para descargar las dependencias</li>
+    </ul>
     <h2>Fuentes bibliograficas:</h2>
-    <p></p>`;
+    <p>Casero, A. (2024, 18 marzo). La importancia del package.json | KeepCoding Bootcamps. KeepCoding Bootcamps. <a target="_blank" href="https://keepcoding.io/blog/la-importancia-del-package-json/">https://keepcoding.io/blog/la-importancia-del-package-json/</a></p>`;
 
 const html_footer = `<footer class="footer">
 <div class="content has-text-centered">

aa35410e76a7ff5ce2d24d792a8ceac5396d9add
YaelChar417 <A01711111@tec.mx>
2025-03-02 18:07:32 -0600

    modelo vistas para ruta /persona4 metodo post hecho

--- routes/persona4.js
+++ routes/persona4.js
@@ -12,20 +12,10 @@ router.get('/',(req, res, next) => {
 // app.post registra un middleware que solo responde a peticiones HTTP tipo post
 router.post('/agregar',(req, res, next) => {
     console.log(req.body);
-
     personaPhotos.push(req.body.urlP4);
-    let html = html_header;
-    html += `<div class="columns">`
-    for(const img of personaPhotos)
-    {
-        html += `<div class="column"><figure class="image is-3by2"><img alt="img no soportada" src="`;
-        html += img;
-        html += `"/></figure></div>`;
-    }
-    html += `</div>`;
-    html += html_footer;
-
-    res.send(html);
+    res.render('lista_persona4', {
+        fotos: personaPhotos,
+    });
 });
 
 module.exports = router;

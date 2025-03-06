6b1e8bafc0799d87af3a6c83fbb7ba95ab4d285f
YaelChar417 <A01711111@tec.mx>
2025-03-06 10:18:14 -0600

    Cookies desplegadas en consola

--- controllers/persona2.js
+++ controllers/persona2.js
@@ -12,10 +12,12 @@ exports.post_agregar = (req, res, next) => {
     console.log(req.body);
     const persona2 = new Persona(req.body.nombre);
     persona2.save();
+    res.setHeader('Set-Cookie', `ultima_sesion=${persona2.nombre}`);
     res.redirect('/persona2/');
 };
 
 exports.get_root = (req, res, next) => {
+    console.log(req.get('Cookie'))
     res.render('lista_persona2', {
         isLoggedIn: req.session.isLoggedIn || false,
         username: req.session.username || '',

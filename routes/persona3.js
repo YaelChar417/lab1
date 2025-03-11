4f9fb96afc5a7c4da4ec867f29b7a4cad1be1e11
YaelChar417 <A01711111@tec.mx>
2025-03-10 20:24:39 -0600

    Feature: Ruta /persona3 protegida con inicio de sesion

--- routes/persona3.js
+++ routes/persona3.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const persona3Crontroller = require('../controllers/persona3');
+const isAuth = require('../util/is-auth');
 
 // app.get registra un middleware que solo responde a peticiones HTTP tipo GET
-router.get('/', persona3Crontroller.get);
+router.get('/', isAuth, persona3Crontroller.get);
 
 module.exports = router;

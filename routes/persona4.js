85743a15ccafb020774f1e4fea0e7690468b2c2d
YaelChar417 <A01711111@tec.mx>
2025-03-12 19:10:33 -0600

    Feature: RBAC implementado para la ruta /persona4

--- routes/persona4.js
+++ routes/persona4.js
@@ -2,15 +2,17 @@ const express = require('express');
 const router = express.Router();
 const persona4Controller = require('../controllers/persona4');
 const isAuth = require('../util/is-auth');
+const canView = require('../util/canViewPersona');
+const canCreate = require('../util/canCreatePersona');
 
 // app.get registra un middleware que solo responde a peticiones HTTP tipo GET
-router.get('/agregar', isAuth, persona4Controller.get_agregar);
+router.get('/agregar', isAuth, canView, persona4Controller.get_agregar);
 
 // app.post registra un middleware que solo responde a peticiones HTTP tipo post
-router.post('/agregar', isAuth, persona4Controller.post_agregar);
+router.post('/agregar', isAuth, canCreate, persona4Controller.post_agregar);
 
-router.get('/:id', isAuth, persona4Controller.get_root);
+router.get('/:id', isAuth, canView, persona4Controller.get_root);
 
-router.get('/', isAuth, persona4Controller.get_root);
+router.get('/', isAuth, canView, persona4Controller.get_root);
 
 module.exports = router;

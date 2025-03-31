be4bcf06467d0c2eaf39fae6cabe69ea545d9d70
YaelChar417 <A01711111@tec.mx>
2025-03-30 22:28:27 -0600

    Fix: personas desplegadas en persona2 despues de crear

--- controllers/persona2.js
+++ controllers/persona2.js
@@ -1,10 +1,10 @@
-const Persona = require('../models/persona2')
+const Persona = require("../models/persona2");
 
 exports.get_agregar = (req, res, next) => {
     console.log(req.session.username);
-    res.render('agregar_persona2', {
+    res.render("agregar_persona2", {
         isLoggedIn: req.session.isLoggedIn || false,
-        username: req.session.username || '',
+        username: req.session.username || "",
         csrfToken: req.csrfToken(),
     });
 };
@@ -12,33 +12,35 @@ exports.get_agregar = (req, res, next) => {
 exports.post_agregar = (req, res, next) => {
     console.log(req.body);
     const persona2 = new Persona(req.body.nombre);
-    persona2.save()
+    persona2
+        .save()
         .then(() => {
             console.log("persona guardada");
             req.session.info = `La persona ${persona2.nombre} ha sido creada con exito`;
-            res.setHeader('Set-Cookie', `ultima_sesion=${persona2.nombre}`);
-            res.redirect('/persona2');
+            res.setHeader("Set-Cookie", `ultima_sesion=${persona2.nombre}`);
+            res.redirect("/persona2");
         })
         .catch((err) => {
             console.log(err);
         });
-    
 };
 
 exports.get_root = (req, res, next) => {
     console.log(req.session.privilegios);
-    const mensaje = req.session.info || '';
+    const mensaje = req.session.info || "";
 
-    if(req.session.info){req.session.info = ''}
+    if (req.session.info) {
+        req.session.info = "";
+    }
 
     Persona.fetch(req.params.id)
         .then(([rows, fieldData]) => {
             console.log(fieldData);
             console.log(rows);
-            res.render('lista_persona2', {
+            res.render("lista_persona2", {
                 isLoggedIn: req.session.isLoggedIn || false,
-                username: req.session.username || '',
-                personas: rows,
+                username: req.session.username || "",
+                personas: rows[0],
                 info: mensaje,
             });
         })

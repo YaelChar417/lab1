47d2c5c59543c9210ad5be56ff708a26ad8b8f70
YaelChar417 <A01711111@tec.mx>
2025-03-30 22:21:48 -0600

    Feature: stored procedures agregados para persona2

--- models/persona2.js
+++ models/persona2.js
@@ -1,32 +1,29 @@
-const db = require('../util/database');
+const db = require("../util/database");
 
 module.exports = class Persona {
-
     //Constructor de la clase. Sirve para crear un nuevo objeto, y en él se definen las propiedades del modelo
     constructor(mi_nombre) {
         this.nombre = mi_nombre;
     }
 
-    //Este método servirá para guardar de manera persistente el nuevo objeto. 
+    //Este método servirá para guardar de manera persistente el nuevo objeto.
     save() {
-        return db.execute('INSERT INTO persona2(nombre) VALUES (?)', [this.nombre]);
+        return db.execute("CALL createPersona2(?)", [this.nombre]);
     }
 
     //Este método servirá para devolver los objetos del almacenamiento persistente.
     static fetchAll() {
-        return db.execute('SELECT * FROM persona2');
+        return db.execute("CALL fetchAllP2()");
     }
 
-    static fetchOne(id){
-        return db.execute('SELECT * FROM persona2 WHERE id = ?', [id]);
+    static fetchOne(id) {
+        return db.execute("CALL fetchOneP2(?)", [id]);
     }
 
-    static fetch(id){
-        if(id)
-        {
+    static fetch(id) {
+        if (id) {
             return this.fetchOne(id);
-        }else
-        {
+        } else {
             return this.fetchAll();
         }
     }

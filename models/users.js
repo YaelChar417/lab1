4d3ac4e29d65de7e5a4ace6cc987bce23a7b81eb
YaelChar417 <A01711111@tec.mx>
2025-04-07 16:08:57 -0600

    Feature: evaluación heuristica con dos casos hecha y commits agregados

--- models/users.js
+++ models/users.js
@@ -1,47 +1,54 @@
-const db = require('../util/database')
-const bcrypt = require('bcryptjs')
+const db = require("../util/database");
+const bcrypt = require("bcryptjs");
 
-module.exports = class User{
-    constructor(username, password){
+module.exports = class User {
+    constructor(username, password) {
         this.username = username;
         this.password = password;
     }
-    save(){
-        return bcrypt.hash(this.password, 12).then((password_cifrado) => {
-            return db.execute('INSERT INTO usuarios(username, password) VALUES (?, ?)', 
-                [this.username, password_cifrado]);
-        })
-        .catch((err) => {
-            console.log(err);
-        });
-    };
-    
-    static fetchAll(){
-        return db.execute('SELECT * FROM usuarios');
+    save() {
+        return bcrypt
+            .hash(this.password, 12)
+            .then((password_cifrado) => {
+                return db.execute(
+                    "INSERT INTO usuarios(username, password) VALUES (?, ?)",
+                    [this.username, password_cifrado]
+                );
+            })
+            .catch((err) => {
+                console.log(err);
+            });
     }
-    
-    static fetchOne(username){
-        return db.execute('SELECT * FROM usuarios WHERE username = ?', [username]);
+
+    static fetchAll() {
+        return db.execute("SELECT * FROM usuarios");
     }
-    
-    static fetch(username){
-        if(username)
-        {
+
+    static fetchOne(username) {
+        return db.execute("SELECT * FROM usuarios WHERE username = ?", [
+            username,
+        ]);
+    }
+
+    static fetch(username) {
+        if (username) {
             return this.fetchOne(username);
-        }else
-        {
+        } else {
             return this.fetchAll();
         }
     }
 
-    static getPrivilegios(username){
-        return db.execute(`SELECT DISTINCT p.nombre 
+    static getPrivilegios(username) {
+        return db.execute(
+            `SELECT DISTINCT p.nombre 
             FROM privilegios AS p, posee AS po,roles AS r, tiene AS t, 
             usuarios AS u
             WHERE p.id = po.privilegio_id 
             AND po.rol_id = r.id 
             AND r.id = t.id_rol
             AND t.id_usuario = u.id 
-            AND username = ?`, [username])
+            AND username = ?`,
+            [username]
+        );
     }
-}
+};
